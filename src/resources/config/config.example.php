f99d20fd91fac3a0b41b755f1c73ca17270cb86f
Adenilton Henrique Ribeiro <adhenriquemusic@gmail.com>
2017-06-28 14:25:39 -0300

    Added folder src/resource/config

---
+++ src/resources/config/config.example.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'version'   => '1.0.0',
+    'headers'          => [
+        'Accept: application/json',
+        'Accept-Charset: utf-8',
+        'Accept-Language: pt-br;q=0.9,pt-BR'
+    ],
+    'defaults'  => [
+        'publishable_key'   => 'YOUR_PUBLISHABLE_KEY',
+        'marketplace_id'    => 'YOUR_MARKETPLACE_ID',
+        'endpoint'          => 'https://api.zoop.ws',
+        'api_version'       => 'v1',
+    ]
+];
