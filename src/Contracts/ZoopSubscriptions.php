04435e5836156244945b445108e6315cbf18ed4a
p_917148 <tacio.brito@camara.leg.br>
2019-07-29 16:17:08 -0300

    Added Subscriptions support

---
+++ src/Contracts/ZoopSubscriptions.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Zoop\Contracts;
+
+use Zoop\Lib\APIResource;
+
+interface ZoopSubscriptions{
+
+    /**
+     * API Resource.
+     *
+     * @param $APIResource $APIResource
+    */
+    public function __construct(APIResource $APIResource);
+
+    /**
+     * Create a new Subscription
+     *
+     * @param $post array
+    */
+    public function create($post);
+
+        /**
+     * Update a created Subscription
+     *
+     * @param $subscriptionID string
+     * @param $post array
+     */
+    public function update($subscriptionID, $post);
+
+    /**
+     * Delete a Subscription by id
+     *
+     * @param $subscriptionID string
+    */
+    public function delete($subscriptionID);
+
+    /**
+     * Suspend a Subscription by id
+     *
+     * @param $subscriptionID string
+    */
+    public function suspend($subscriptionID);
+
+    /**
+     * Reactivate a Subscription by id
+     *
+     * @param $subscriptionID string
+    */
+    public function reactivate($subscriptionID);
+
+    /**
+     * Retrieve the details of a Subscription by id
+     *
+     * @param $subscriptionID string
+     */
+    public function get($subscriptionID);
+
+    /**
+     * Returns a JSON object with a list of Subscriptions.
+     */
+    public function getAll();
+}
--- src/ZoopServiceProvider.php
+++ src/ZoopServiceProvider.php
@@ -14,6 +14,7 @@ use Zoop\Lib\ZoopSplitTransactions;
 use Zoop\Lib\ZoopTokens;
 use Zoop\Lib\ZoopTransfers;
 use Zoop\Lib\ZoopPlans;
+use Zoop\Lib\ZoopSubscriptions;
 
 class ZoopServiceProvider extends ServiceProvider {
 
@@ -52,23 +53,27 @@ class ZoopServiceProvider extends ServiceProvider {
         });
 
         $this->app->singleton('ZoopSellers', function () use ($service) {
-                return new ZoopSellers(APIResource::getSingleton($service));
+            return new ZoopSellers(APIResource::getSingleton($service));
         });
 
         $this->app->singleton('ZoopSplitTransactions', function () use ($service) {
-                return new ZoopSplitTransactions(APIResource::getSingleton($service));
+            return new ZoopSplitTransactions(APIResource::getSingleton($service));
         });
 
         $this->app->singleton('ZoopTokens', function () use ($service) {
-                return new ZoopTokens(APIResource::getSingleton($service));
+            return new ZoopTokens(APIResource::getSingleton($service));
         });
 
         $this->app->singleton('ZoopTransfers', function () use ($service) {
-                return new ZoopTransfers(APIResource::getSingleton($service));
+            return new ZoopTransfers(APIResource::getSingleton($service));
         });
 
         $this->app->singleton('ZoopPlans', function () use ($service) {
-                return new ZoopPlans(APIResource::getSingleton($service));
+            return new ZoopPlans(APIResource::getSingleton($service));
+        });
+
+        $this->app->singleton('ZoopSubscriptions', function () use ($service) {
+            return new ZoopSubscriptions(APIResource::getSingleton($service));
         });
     }
 
@@ -85,6 +90,7 @@ class ZoopServiceProvider extends ServiceProvider {
             ZoopTokens::class,
             ZoopTransfers::class,
             ZoopPlans::class
+            ZoopSubscriptions::class
         ];
     }
 
